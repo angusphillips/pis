50081bcad4decffee44c2b0f46e175d5680fe417
angusphillips <angus.phillips@stats.ox.ac.uk>
2024-01-09 18:54:15 +0000

    config updates for first ablations

--- src/callbacks/metric_cb.py
+++ src/callbacks/metric_cb.py
@@ -8,7 +8,7 @@ from src.utils.sampling import generate_samples_loss
 
 # pylint: disable=no-self-use, unnecessary-pass
 class VizSampleDist(Callback):
-    def __init__(self, every_n, num_sample=6000):
+    def __init__(self, every_n, num_sample=2000):
         self.every_n = every_n
         self.num_sample = num_sample
 
